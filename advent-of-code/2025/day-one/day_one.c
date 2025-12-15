8551d2f6b4ece22af3dfd9f63a6669f2ff83c9e7
Brady Dame <bdame2@huskers.unl.edu>
2025-12-15 08:46:38 +0000

    file restructure

--- advent-of-code/2025/day-one/day_one.c
+++ advent-of-code/2025/day-one/day_one.c
@@ -1,3 +1,10 @@
+/**
+ * Author: Brady Dame
+ * Date: 2025-12-11
+ * 
+ * This program counts the number of times a dial lands on '0' when trying to 
+ * unlock the safe for Advent of Code - 2025, Day One Puzzle
+ */
 #include <stdlib.h>
 #include <stdio.h>
 
@@ -7,7 +14,9 @@ int main(int argc, char** argv) {
 
     char buffer[10];
     while(fgets(buffer, sizeof(buffer), f) != NULL) {
-        printf("%s", buffer);
+        char direction = buffer[0];
+        int number = atoi(&buffer[1]);
+        printf("Action: %sDirection: %c, Clicks: %d\n", buffer, direction, number);
     }
 
     fclose(f);
